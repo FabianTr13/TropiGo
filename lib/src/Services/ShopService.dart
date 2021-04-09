1937a5ba2ee4fdd6c97c1472a929db596749cf4a
christian_sc <cdsanchezc96@gmail.com>
2021-04-09 16:45:11 -0600

    Adding parameter cityId in prodyct request
    Adding parameter cityId in prodyct request

--- lib/src/Services/ShopService.dart
+++ lib/src/Services/ShopService.dart
@@ -16,67 +16,22 @@ class ShopService {
   final databaseReference = FirebaseDatabase.instance.reference();
 
   Future<List<Product>> getProducts() async {
-    http.Response response = await http
-        .get("http://apitropigas.hol.es/apiKio/public/api/productos/34");
+    String cityId = await getCityId() ?? "1";
 
-    var data = json.decode(response.body);
-    // var data = [
-    //   {
-    //     "codProducto": "43",
-    //     "exento": 1,
-    //     "nombreProducto": "Gas 10LB",
-    //     "impuesto": 0,
-    //     "precio": "107.07",
-    //     "manejaInventario": "0",
-    //     "precioDigitadoManual": "0",
-    //     "cantidad": "0"
-    //   },
-    //   {
-    //     "codProducto": "44",
-    //     "exento": 1,
-    //     "nombreProducto": "Gas 20LB",
-    //     "impuesto": 0,
-    //     "precio": "214.59",
-    //     "manejaInventario": "0",
-    //     "precioDigitadoManual": "0",
-    //     "cantidad": "0"
-    //   },
-    //   {
-    //     "codProducto": "31",
-    //     "exento": 1,
-    //     "nombreProducto": "Gas 25LB",
-    //     "impuesto": 0,
-    //     "precio": "215.46",
-    //     "manejaInventario": "0",
-    //     "precioDigitadoManual": "0",
-    //     "cantidad": "0"
-    //   },
-    //   {
-    //     "codProducto": "39",
-    //     "exento": 1,
-    //     "nombreProducto": "Gas 60LB",
-    //     "impuesto": 0,
-    //     "precio": "635.29",
-    //     "manejaInventario": "0",
-    //     "precioDigitadoManual": "0",
-    //     "cantidad": "0"
-    //   },
-    //   {
-    //     "codProducto": "40",
-    //     "exento": 1,
-    //     "nombreProducto": "Gas 100LB",
-    //     "impuesto": 0,
-    //     "precio": "1170.61",
-    //     "manejaInventario": "0",
-    //     "precioDigitadoManual": "0",
-    //     "cantidad": "0"
-    //   }
-    // ];
+    final Response response = await http
+        .get("http://apitropigas.hol.es/apiKio/public/api/productos/$cityId");
 
-    shopCylinderBlocInstance.changeProductSelect(null);
-    List<Product> products =
-        data.map<Product>((item) => new Product(item)).toList();
-    shopCylinderBlocInstance.changeProduct(products);
+    List<Product> products = [];
+    if (response.statusCode == 200) {
+      final dynamic data = json.decode(response.body);
+
+      bool validateData = data is String;
+      if (!validateData) {
+        shopCylinderBlocInstance.changeProductSelect(null);
+        products = data.map<Product>((item) => Product(item)).toList();
+        shopCylinderBlocInstance.changeProduct(products);
+      }
+    }
     return products;
   }
 
